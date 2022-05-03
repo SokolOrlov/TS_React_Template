36964124eb876383d73d583a8b0f8c000801cedd
Andrey <topt2@ya.ru>
2022-05-03 20:23:45 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry:{
+    main:'./src/App.tsx'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode:"development",
+  module: {
+    rules: [
+      {test: /\.tsx?$/,use: 'ts-loader',exclude: /node_modules/},
+      { test: /\.css$/, use: ['style-loader','css-loader'] }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: './dist',
+  },
+   plugins: [
+     new HtmlWebpackPlugin({template: './src/index.html'}),
+   ],
+};
