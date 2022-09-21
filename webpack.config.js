cfb2057ad85b09f13ecc3645f95e7a52992b3840
Andrey <topt2@ya.ru>
2022-09-21 11:44:01 +0300

    fix load module.css

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,10 @@ module.exports = {
   module: {
     rules: [
       {test: /\.tsx?$/,use: 'ts-loader',exclude: /node_modules/},
-      { test: /\.css$/, use: ['style-loader','css-loader'] }
+      { test: /\.css$/, use: [
+        { loader: 'style-loader'},
+        {loader: 'css-loader', options: { modules: true }}] 
+      }
     ],
   },
   resolve: {
